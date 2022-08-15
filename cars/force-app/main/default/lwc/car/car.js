3a46abb8f38978aee34ab8dba20151f16283d2bb
Maryo Botros <maryo.botros@farmersinsurance.com>
2022-08-15 12:09:24 -0700

    Dispatching and Handling Events

--- cars/force-app/main/default/lwc/car/car.js
+++ cars/force-app/main/default/lwc/car/car.js
@@ -2,4 +2,10 @@ import { LightningElement, api } from 'lwc';
 
 export default class Car extends LightningElement {
     @api carInfo = {carMake: "Toyota", carModel: "Corolla"};
+
+    tileClickHandler(){
+        const tileClicked = new CustomEvent("tileclick", {detail: this.carInfo});
+
+        this.dispatchEvent(tileClicked);
+    }
 }
--- cars/force-app/main/default/lwc/cars/cars.js
+++ cars/force-app/main/default/lwc/cars/cars.js
@@ -1,10 +1,17 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, track } from 'lwc';
 
 export default class Cars extends LightningElement {
+    @track selectedCar;
+    
     carsInfo = [
         {carMake: "Tesla", carModel: "Model S"},
         {carMake: "Honda", carModel: "Accord"},
         {carMake: "Mercedes", carModel: "S Class"},
         {carMake: "Chevy", carModel: "Silverado"}
     ];
+
+    onTileSelectHandler(event){
+        const carInfo = event.detail;
+        this.selectedCar = carInfo.carMake;
+    }
 }
